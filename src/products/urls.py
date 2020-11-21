90e329cefdde12440d3e325b840dfdd20469b1b3
KTM28 <vabishya.gautam@gmail.com>
2020-11-21 17:31:18 +0000

    views and template to display all service items added

--- src/products/urls.py
+++ src/products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('services/', views.all_services, name='services'),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -6,9 +6,23 @@ def all_products(request):
     
     all_products = Product.objects.filter(is_a_treatment=False)
     active_products = all_products.filter(discontinued=False)
+    
+    query = None
+    categories = None
 
     context = {
         'products': all_products,
     }
 
     return render(request, 'products/all_products.html', context)
+
+def all_services(request):
+    """ A view to show all the services """
+
+    all_services = Product.objects.filter(is_a_treatment=True)
+
+    context = {
+        'services': all_services,
+    }
+
+    return render(request, 'products/all_services.html', context)
