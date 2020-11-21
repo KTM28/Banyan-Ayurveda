b4a61a2f8375faac49a666a214369365ec4d175b
KTM28 <vabishya.gautam@gmail.com>
2020-11-21 13:27:31 +0000

    search form added to product templates

--- src/products/views.py
+++ src/products/views.py
@@ -4,7 +4,8 @@ from .models import Product, Category
 def all_products(request):
     """ A view to show all products with it's sort and search queries"""
     
-    all_products = Product.objects.all()
+    all_products = Product.objects.filter(is_a_treatment=False)
+    active_products = all_products.filter(discontinued=False)
 
     context = {
         'products': all_products,
