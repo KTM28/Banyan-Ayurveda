361d9b2a74e8f968d7fa1ca98e5469130ef9a1bd
KTM28 <vabishya.gautam@gmail.com>
2020-11-26 12:33:05 +0000

    all_service.html cards modified to fit the layout

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-   
+    $('.service-info').each(function (f) {
+
+        var newstr = $(this).text().substring(0, 350);
+        $(this).text(newstr + '.....');
+
+    });
 });
