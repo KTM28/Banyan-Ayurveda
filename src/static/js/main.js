59bc2a6a877c2cd8d74b47de1fc0978da6be2680
KTM28 <vabishya.gautam@gmail.com>
2020-12-10 13:40:47 +0000

    Datetime picker js initiailization issue for not picking date fixed

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -4,12 +4,15 @@ $(document).ready(function () {
         $(this).text(newstr + '.....');
     });
     const dateTimePickerInit = () => {
-        $('#datetime').datetimepicker({
+        $('#datetime, div[id^=cart_datetime-]').datetimepicker({
             format: 'DD/MM/YYYY HH:mm',
             minDate: new Date(),
             enabledHours: [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
             stepping: 30,
         });
+        $('div[id^=cart_datetime-]').each(function () {
+            $(this).children('input.datetimepicker-input').val($(this).data('value'));
+          });
     };
     dateTimePickerInit();
 });
