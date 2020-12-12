063ba5361da73c140b3718baed22d8cdf41d52af
KTM28 <vabishya.gautam@gmail.com>
2020-12-12 15:03:38 +0000

    Added view for update and remove cart

--- src/banyan_ayurveda/settings.py
+++ src/banyan_ayurveda/settings.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'banyan_ayurveda.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- src/cart/views.py
+++ src/cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from products.models import Product
 
 def view_cart(request):
@@ -36,3 +36,42 @@ def add_to_cart(request, item_id):
     request.session['cart'] = cart
     return redirect(redirect_url)
     
+
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    product = Product.objects.get(pk=item_id)
+    cart = request.session.get('cart', {})
+    datetime = None
+    if 'datetime' in request.POST:
+        datetime = request.POST['datetime']
+    if datetime:
+        if quantity > 0:
+            cart[item_id] = {'items_by_datetime': {datetime: quantity}}
+        else:
+            del cart[item_id]['items_by_datetime'][datetime]
+            if not cart[item_id]['items_by_datetime']:
+                cart.pop(item_id)
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping cart"""
+    product = Product.objects.get(pk=item_id)
+
+    try:
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
