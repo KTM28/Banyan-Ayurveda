c66868b1b303a6cb0169a88495e53fc1a3261601
KTM28 <vabishya.gautam@gmail.com>
2020-12-13 19:19:48 +0000

    Added Toasts

--- src/cart/views.py
+++ src/cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
 from products.models import Product
 
 def view_cart(request):
@@ -31,6 +32,7 @@ def add_to_cart(request, item_id):
             cart[item_id] += quantity
         else:
             cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your cart')
     
     # Overwrite the variable in the session with the updated version
     request.session['cart'] = cart
