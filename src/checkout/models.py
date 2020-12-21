a764de3e997ab62d7c22f3305f4a305a4f49a9bc
KTM28 <vabishya.gautam@gmail.com>
2020-12-21 19:47:31 +0000

    Updated checkout form

--- src/checkout/models.py
+++ src/checkout/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
+from django_countries.fields import CountryField
 
 from products.models import Product
 
@@ -18,7 +19,7 @@ class Order(models.Model):
     address_line2 = models.CharField(max_length=60, null=True, blank=True)
     town_or_city = models.CharField(max_length=50, null=False, blank=False)
     county = models.CharField(max_length=50, null=True, blank=True)
-    country = models.CharField(max_length=50, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     purchase_date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2,
