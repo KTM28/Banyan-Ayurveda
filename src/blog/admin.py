13a5e33b04fb0376a7138d7d5f7622b63457d982
KTM28 <vabishya.gautam@gmail.com>
2021-01-07 11:56:52 +0000

    added models like comment and view for blog and registered in the admin

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Blog, BlogView, Comment, Like, Category
+
+class BlogListAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author',
+                    'category',)
+    list_filter = ('author',
+                'category',)
+
+admin.site.register(Blog, BlogListAdmin)
+admin.site.register(BlogView)
+admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(Like)
+
