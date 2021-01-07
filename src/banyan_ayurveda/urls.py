63cc0eea7378a5897365c6bdd22c18507d49a4ef
KTM28 <vabishya.gautam@gmail.com>
2021-01-07 12:10:27 +0000

    Added views and urls for blogs

--- src/banyan_ayurveda/urls.py
+++ src/banyan_ayurveda/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('blog/', include('blog.urls')),
     path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Blog, BlogView, Like, Comment, Category
 
-# Create your views here.
+
+class BlogListView(ListView):
+    model = Blog
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+class BlogCreateView(CreateView):
+    model = Blog
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    
