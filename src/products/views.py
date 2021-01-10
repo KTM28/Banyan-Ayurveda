b23ce1ae166bc973e7745f35dd94085fdb780904
KTM28 <vabishya.gautam@gmail.com>
2021-01-10 18:47:41 +0000

    added edit product view/template and edit service template

--- src/products/views.py
+++ src/products/views.py
@@ -108,7 +108,7 @@ def add_service(request):
         messages.error(request, 'Access denied!\
             Only store owners can do that!')
         return redirect(reverse('landing'))
-        
+
     if request.method == 'POST':
         form = ServiceForm(request.POST, request.FILES)
         if form.is_valid():
@@ -125,6 +125,35 @@ def add_service(request):
     template = 'products/add_service.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ A view to allow admin to edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Access denied!\
+            Only store owners can edit products.')
+        return redirect(reverse('landing'))
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_details', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product.\
+                Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+        
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
