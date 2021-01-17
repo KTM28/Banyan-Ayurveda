796fe8e7d17265cb843a1d9bbc40806e85347e0d
KTM28 <vabishya.gautam@gmail.com>
2021-01-17 10:25:18 +0000

    re-added the app newsletters

--- src/banyan_ayurveda/urls.py
+++ src/banyan_ayurveda/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('blog/', include('blog.urls')),
     path('contact/', include('contact.urls')),
+    path('newsletters/', include('newsletters.urls', namespace='newsletters')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
